4f48879df4ee7af985572193e7a47adf692b248b
Diego Fialho Rodrigues <diego.fialho@ufv.br>
2025-04-25 11:49:07 -0300

    3-21 Linkedin data processing - part 5: Custom search agent testing

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,10 +6,11 @@ from dotenv import load_dotenv
 import os
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-if __name__ == '__main__':
-    load_dotenv()
-    print("Loading...")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=False)
     summary_template = """
         given the information {information} about a person from I want you to create:
         1. a short summary
@@ -20,13 +21,18 @@ if __name__ == '__main__':
         input_variables=["information"],
         template=summary_template,
     )
-    # llm = ChatOpenAI(
-    #     model_name="gpt-3.5-turbo",
-    #     temperature=0,
-    # )
-    llm = ChatOllama(model="llama3",)
+    llm = ChatOpenAI(
+        model_name="gpt-4o-mini",
+        temperature=0,
+    )
+    # llm = ChatOllama(model="llama3",)
     # llm = ChatOllama(model="mistral",)
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/diego-fialho/", mock=True)
+    chain = summary_prompt_template | llm
     res = chain.invoke(input={"information": linkedin_data})
     print(res)
+
+if __name__ == '__main__':
+    load_dotenv()
+    print("Ice Breaker")
+    print("Loading...")
+    ice_break_with(name="Diego Fialho")
